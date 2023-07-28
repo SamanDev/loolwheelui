2e5d59883c7116de7b77d95d327d276ebb0cf00d
SamanDev <mac.developers7@gmail.com>
2023-07-29 00:46:22 +0330

    ss

--- src/adscom.js
+++ src/adscom.js
@@ -1,20 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const AdsComponent = (props) => {
   const { dataAdSlot } = props;
-
+  const [show, setShow] = useState(false);
   useEffect(() => {
     try {
       (window.adsbygoogle = window.adsbygoogle || []).push({});
+      setShow(true);
     } catch (e) {
+      setShow(false);
       setTimeout(() => {
         try {
           (window.adsbygoogle = window.adsbygoogle || []).push({});
-        } catch (error) {}
-      }, 500);
+          setShow(true);
+        } catch (e) {
+          setShow(false);
+        }
+      }, 100);
     }
   }, []);
-
+  if (!show) {
+    return (
+      <ins
+        className="adsbygoogle"
+        style={{ display: "none", width: 300, height: 600 }}
+        data-ad-client="ca-pub-7264153250850834"
+        data-ad-slot={dataAdSlot}
+      ></ins>
+    );
+  }
   return (
     <>
       <ins
