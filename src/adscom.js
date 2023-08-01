1f51451df0a1a6e790a694f90ec56851d74db8c6
SamanDev <mac.developers7@gmail.com>
2023-08-01 07:07:54 +0330

    ss

--- src/adscom.js
+++ src/adscom.js
@@ -5,6 +5,17 @@ import { AdScript, AdConfig } from "react-ad-manager";
 const AdsComponent = (props) => {
   return (
     <>
+      <AdConfig
+        networkCode={1}
+        target={[["global", "true"]]}
+        collapseEmptyDivs={true}
+        eventImpressionViewable={(e) => console.log(e.slot)}
+        eventSlotOnload={(e) => console.log(e.slot)}
+        eventSlotRenderEnded={(e) => console.log(e.slot)}
+        eventSlotRequested={(e) => console.log(e.slot)}
+        eventSlotResponseReceived={(e) => console.log(e.slot)}
+        eventSlotVisibilityChanged={(e) => console.log(e.slot)}
+      />
       <Ad
         adUnit="/22964122449/reward"
         name="div-gpt-ad-1690857849617-0"
