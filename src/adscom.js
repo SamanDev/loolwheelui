555203af095589d6c715417d6736e9202d60e28f
SamanDev <mac.developers7@gmail.com>
2023-08-01 07:13:47 +0330

    ss

--- src/adscom.js
+++ src/adscom.js
@@ -5,27 +5,22 @@ import { AdScript, AdConfig } from "react-ad-manager";
 const AdsComponent = (props) => {
   return (
     <>
-      <AdConfig
-        networkCode={1}
-        target={[["global", "true"]]}
-        collapseEmptyDivs={true}
-        eventImpressionViewable={(e) => console.log(e.slot)}
-        eventSlotOnload={(e) => console.log(e.slot)}
-        eventSlotRenderEnded={(e) => console.log(e.slot)}
-        eventSlotRequested={(e) => console.log(e.slot)}
-        eventSlotResponseReceived={(e) => console.log(e.slot)}
-        eventSlotVisibilityChanged={(e) => console.log(e.slot)}
-      />
+      <AdConfig networkCode={1} target={[["global", "true"]]} />
       <Ad
         adUnit="/22964122449/reward"
         name="div-gpt-ad-1690857849617-0"
-        size={["fluid"]}
-        eventImpressionViewable={(e) => console.log(e.slot)}
-        eventSlotOnload={(e) => console.log(e.slot)}
-        eventSlotRenderEnded={(e) => console.log(e.slot)}
-        eventSlotRequested={(e) => console.log(e.slot)}
-        eventSlotResponseReceived={(e) => console.log(e.slot)}
-        eventSlotVisibilityChanged={(e) => console.log(e.slot)}
+        size={[
+          [
+            [1024, 768],
+            [
+              [750, 200],
+              [728, 90],
+            ],
+          ],
+          [[640, 480], [[300, 250]]],
+        ]}
+        refreshTimer={5000}
+        type="INTERSTITIAL"
       />
     </>
   );
